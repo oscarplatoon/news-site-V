5af67e84d1313125c8c287c5bb696928e6e70e52
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-27 16:53:23 -0400

    class based Article List

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import ArticleTeaser from '../ArticleTeaser/ArticleTeaser.js';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const ArticleList = (props) => {
+class ArticleList extends Component {
   
-  const { articles } = props;
-  return (
-    <ListGroup className='shadow'>
-      { articles.map((article, index) => (
-        <ListGroupItem  key={index}>
-          <ArticleTeaser { ...article } />
-          {/* <ArticleTeaser { ...article } id={ article.id } /> */}
-        </ListGroupItem>
-      ))}
-    </ListGroup>
-  );
+  render() {
+    const { articles } = this.props;
+    return (
+      <ListGroup className='shadow'>
+        { articles.map((article, index) => (
+          <ListGroupItem  key={index}>
+            <ArticleTeaser { ...article } />
+            {/* <ArticleTeaser { ...article } id={ article.id } /> */}
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    );
+  }
 }
 
 export default ArticleList;
