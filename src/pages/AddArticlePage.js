a92a4b67b4ef4f5eee6bd915e437a20f86c4ef4f
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-27 17:02:39 -0400

    class based add article page.

--- src/pages/AddArticlePage.js
+++ src/pages/AddArticlePage.js
@@ -1,65 +1,72 @@
-import {useState} from 'react'
+import {Component} from 'react'
 import { Form, FormGroup, Button, Input, Label } from "reactstrap"
 import ArticlesAPI from "../api/ArticlesAPI"
 
-const AddArticlePage = () => {
-
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const [submissionSuccess, setSubmissionSuccess] = useState(false)
-  const [subMessage, setSubMessage] = useState('')
-
-  const handleForm = (evt) => {
+class AddArticlePage extends Component {
+  state = {
+    isSubmitted:false,
+    submissionSuccess:false,
+    subMessage: ""
+    }
+  handleForm = (evt) => {
     evt.preventDefault();
     let articleObject = {
-      'title':evt.target.title.value,
-      'byline':evt.target.byline.value,
-      'abstract':evt.target.abstract.value
+      'title': evt.target.title.value,
+      'byline': evt.target.byline.value,
+      'abstract': evt.target.abstract.value
     }
-    setIsSubmitted(true)
-    ArticlesAPI.addArticle(articleObject)
-    .then((json) => {
-      if(json.isError == false) {
-        setSubmissionSuccess(true)
-        setSubMessage(json.message)
-      } else if(json.isError == true) {
-        setSubMessage(json.message)
-
-      }
-      console.log(json.isError)
+    setState({
+      isSubmitted:true,
     })
+    ArticlesAPI.addArticle(articleObject)
+      .then((json) => {
+        if (json.isError == false) {
+          setState({
+            SubmissionSuccess:true,
+            subMessage:json.message
+          })
+        } else if (json.isError == true) {
+          setState({
+            subMessage:json.message
+          })
+        }
+        console.log(json.isError)
+      })
   }
 
 
-  return(
-    <div>
-      { isSubmitted && submissionSuccess?
-        <h4 className='bg-success text-dark'> {subMessage}</h4>
-        : !isSubmitted ? <h4 className='bg-secondary text-light'> Please fill out the form: </h4>
-        : <h4 className='bg-danger text-warning'>{subMessage}</h4>
-      }
-      <Form onSubmit={handleForm}>
-        <FormGroup>
-          <Label>
-            Title:
-          </Label>
-          <Input required type='text' name='title' placeholder='Rabid puppies arise from the depths' />
-        </FormGroup>
-        <FormGroup>
-          <Label>
-            Byline:
-          </Label>
-          <Input required type='text' name='byline' placeholder='By: Dr. So-and-so' />
-        </FormGroup>
-        <FormGroup>
-          <Label>
-            Abstract:
-          </Label>
-          <Input required type='textarea' name='abstract' placeholder='Article Abstract Goes Here' />
-        </FormGroup>
-        <Button color='success' type='submit'>Submit Article</Button>
-      </Form>
-    </div>
-  )
+  render() { 
+    return ( 
+      <div>
+        { this.state.isSubmitted && this.state.submissionSuccess?
+          <h4 className='bg-success text-dark'> {this.state.subMessage}</h4>
+          : !this.state.isSubmitted ? <h4 className='bg-secondary text-light'> Please fill out the form: </h4>
+          : <h4 className='bg-danger text-warning'>{this.state.subMessage}</h4>
+        }
+        <Form onSubmit={this.handleForm}>
+          <FormGroup>
+            <Label>
+              Title:
+            </Label>
+            <Input required type='text' name='title' placeholder='Rabid puppies arise from the depths' />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              Byline:
+            </Label>
+            <Input required type='text' name='byline' placeholder='By: Dr. So-and-so' />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              Abstract:
+            </Label>
+            <Input required type='textarea' name='abstract' placeholder='Article Abstract Goes Here' />
+          </FormGroup>
+          <Button color='success' type='submit'>Submit Article</Button>
+        </Form>
+      </div>
+     );
+  }
 }
-
-export default AddArticlePage
+ 
+export default AddArticlePage;
