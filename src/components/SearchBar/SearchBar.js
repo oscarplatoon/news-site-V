2fd41c45e9142820730a93aa76421f19a354055b
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-27 16:57:07 -0400

    Class based Search Bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,26 +1,36 @@
-import { useEffect, useState } from 'react'
-import {Button, Form, FormGroup, Input, InputGroup, InputGroupAddon } from 'reactstrap' 
+import { Component } from 'react'
+import {
+  Button,
+  Form,
+  FormGroup, 
+  Input, 
+  InputGroup, 
+  InputGroupAddon 
+} from 'reactstrap' 
 
-const SearchBar = (props) => {
 
-  const handleSearch = (evt) => {
+class SearchBar extends Component {
+  handleSearch = (evt) => {
     evt.preventDefault()
     console.log(evt.target.input.value)
-    props.handleFilterText(evt.target.input.value)
+    this.props.handleFilterText(evt.target.input.value)
   }
 
-  return (
-    <Form className='form-inline' onSubmit={handleSearch}>
-      <FormGroup>
-        <InputGroup>
-          <InputGroupAddon addonType='prepend' type='submit'>
-            <Button color='secondary'>Search</Button>
-          </InputGroupAddon>
-          <Input name='input' type="text" placeholder="Filter Articles" />
-        </InputGroup>
-      </FormGroup>
-    </Form>
-  );
+  render() { 
+    return (
+      <Form className='form-inline' onSubmit={this.handleSearch}>
+        <FormGroup>
+          <InputGroup>
+            <InputGroupAddon addonType='prepend' type='submit'>
+              <Button color='secondary'>Search</Button>
+            </InputGroupAddon>
+            <Input name='input' type="text" placeholder="Filter Articles" />
+          </InputGroup>
+        </FormGroup>
+      </Form>
+    );
+  }
 }
+ 
  
 export default SearchBar;
