62c9be1bca5dbf8f9fe4bfae806edd8487642484
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-27 16:54:21 -0400

    Class based article teaser

--- src/components/ArticleTeaser/ArticleTeaser.js
+++ src/components/ArticleTeaser/ArticleTeaser.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const ArticleTeaser = (props) => {
+class ArticleTeaser extends Component {
   
-
-  const { id, title, created_date: createdDate } = props;
-  return (
-    <div>
-      <ListGroupItemHeading >
-        <Link  to={`/articles/${id}`}>{title}</Link>
-      </ListGroupItemHeading>
-      <ListGroupItemText>{ createdDate }</ListGroupItemText>
-    </div>
-  )
+  render() {
+    const { id, title, created_date: createdDate } = this.props;
+    return (
+      <div>
+        <ListGroupItemHeading >
+          <Link  to={`/articles/${id}`}>{title}</Link>
+        </ListGroupItemHeading>
+        <ListGroupItemText>{ createdDate }</ListGroupItemText>
+      </div>
+    )
+  }
 
 }
 
