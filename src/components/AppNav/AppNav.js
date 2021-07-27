9ec3680a070218fd748a39c72d33a6da0dde7f86
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-27 16:52:07 -0400

    class based AppNav

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -1,39 +1,40 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Button, Navbar, Nav, NavItem } from 'reactstrap';
 import navItems from '../../config/Sections.json';
 import SearchBar from '../SearchBar/SearchBar';
 
 
-const AppNav = (props) => {
-  
-  return (
-    <Navbar color="light">
-      <Nav >
-        <Link to='/'>| Home |</Link>
-      </Nav>
-      <Nav>
-        {
-          navItems.map((navItem, index) =>
-            <Link to={`/sections/${navItem.label.toLowerCase()}`} key={index}>
-              | { navItem.label } |
-            </Link>
-        )}
-      </Nav>
-        < SearchBar handleFilterText={props.handleFilterText}/>
-      <NavLink to='/add-article' >
-        <Button color='danger' className=''>
-            Add Article
-        </Button>
-      </NavLink>
-      <NavLink to='/login'>
-        <Button color='success' classname='text-info btn-success'>
-          Login
-        </Button>
-      </NavLink>
-    </Navbar>
-  )
+class AppNav extends Component {
+  render() {
+    return (
+      <Navbar color="light">
+        <Nav >
+          <Link to='/'>| Home |</Link>
+        </Nav>
+        <Nav>
+          {
+            navItems.map((navItem, index) =>
+              <Link to={`/sections/${navItem.label.toLowerCase()}`} key={index}>
+                | { navItem.label } |
+              </Link>
+          )}
+        </Nav>
+          < SearchBar handleFilterText={this.props.handleFilterText}/>
+        <NavLink to='/add-article' >
+          <Button color='danger' className=''>
+              Add Article
+          </Button>
+        </NavLink>
+        <NavLink to='/login'>
+          <Button color='success' classname='text-info btn-success'>
+            Login
+          </Button>
+        </NavLink>
+      </Navbar>
+    )
+  }
 }
 
 export default AppNav;
