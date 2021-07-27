bcf0f752b6c2241db68465565f9651dbf647f90b
tomprete <prete316@gmail.com>
2021-07-27 11:57:16 -0500

    instructor solution

--- src/api/ArticlesAPI.js
+++ src/api/ArticlesAPI.js
@@ -1,33 +1,76 @@
-const BASE_URL = 'http://localhost:3001/api/articles';
+const URL = 'http://localhost:3001/api/articles'
 
 const fetchArticleByID = async (articleID) => {
-  const response = await fetch(`${BASE_URL}/${articleID}`);
-  const data = await response.json();
-  return data;
-};
+  try {
+    let response = await fetch(`${URL}/${articleID}`)
+    let data = await response.json()
+    return data
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
 
 const fetchArticlesBySection = async (section) => {
-  const response = await fetch(`${BASE_URL}?filter={"where":{"section":"${section}"}}`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${URL}?filter={"where":{"section":"${section}"}}`);
+    const data = await response.json();
+    return data;
+  }
+  catch(err) {
+    console.error('There was an error.')
+  }
 };
 
-const fetchArticles = async (filters = null) => {
-  const url = filters ? `${BASE_URL}?filter={"where":${filters}}` : BASE_URL;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
-
-const searchArticles = async (textToSearchFor) => {
-  const response = await fetch(`${BASE_URL}?filter={"where":{"title":{"ilike":"${textToSearchFor}"}}}`)
-  const data = await response.json();
-  return data;
+const fetchArticles = (filters = null) => {
+  console.log('Fetch Articles')
+  try {
+    return fetch(`${URL}${filters !== null ? `?filter={"where":{"title":{"ilike":"${filters}"}}}` : ''}`)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  catch(err) {
+    console.error(err)
+  }
 }
+
+const addArticle = async (articleObject) => {
+  try {
+    let response = await fetch(URL, {
+      headers: {
+        'Content-type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(articleObject)
+    })
+    let data = await response.json();
+    if (data.error) {
+      return {
+        'message': 'There was an error saving your message. Please make sure all fields are filled.',
+        'isError': true,
+        'statusCode': 200
+      }
+    }
+    else {
+      return {
+        'message': 'Article Successfully Created',
+        'isError': false,
+        'statusCode': 200
+      }
+    }
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+
 
 export {
   fetchArticleByID,
   fetchArticles,
   fetchArticlesBySection,
-  searchArticles,
+  addArticle
 };
--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'reactstrap';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import navItems from '../../config/Sections.json';
+import { withRouter } from "react-router";
 
-class AppNav extends Component {
-  render() {
-    return (
-      <Navbar color="light">
-        {navItems.map((navItem) =>
-          <Link to={`/sections/${navItem.value}`} >
-            { navItem.label }
-          </Link>
-        )}
-      </Navbar>
-    )
+// --- Class Based Component
+// class AppNav extends Component {
+
+//   render() {
+//     console.log(this.props)
+
+//     const createNavItems = () => {
+//       return navItems.map((navItem, idx) => {
+//         return <Nav.Link key={idx} onClick={() => this.props.history.push(`/sections/${navItem.value}`)}>{ navItem.label }</Nav.Link>
+//       })
+//     }
+
+//     return (
+//       <Navbar bg="dark" variant="dark">
+//           <Navbar.Brand href="/" className='m-2'>News Site</Navbar.Brand>
+//             <Nav className="me-auto">
+//             {
+//               createNavItems()
+//             }
+//             </Nav>
+//             <Form className='d-flex' onSubmit={ this.props.handleSearch }>
+//               <FormControl type="search" placeholder="Search" className="mr-2" name='search' />
+//               <Button type='submit' variant="outline-success">Search</Button>
+//             </Form>
+//       </Navbar>
+//     )
+//   }
+// }
+
+// --- Functional Component
+const AppNav = (props) => {
+  const { handleSearch } = props;
+
+
+  const createNavItems = () => {
+    return navItems.map((navItem, idx) => {
+      return <Nav.Link key={idx} onClick={() => props.history.push(`/sections/${navItem.value}`)}>{navItem.label}</Nav.Link>
+    })
   }
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/" className='m-2'>News Site</Navbar.Brand>
+      <Nav className="me-auto">
+        {
+          createNavItems()
+        }
+        <Button href='/login' variant='outline-primary' className='mx-2'>Login</Button>
+        <Button href='/add-article' variant='outline-secondary' className='mx-2'>Add Article</Button>
+      </Nav>
+      <Form className='d-flex' onSubmit={handleSearch}>
+        <FormControl type="search" placeholder="Search" className="mr-2" name='search' />
+        <Button type='submit' variant="outline-success">Search</Button>
+      </Form>
+    </Navbar>
+  )
 }
 
-export default AppNav;
-
-
-// Functional solution:
-// function AppNav() {
-//   return (
-//     <Navbar color="light">
-//       {navItems.map((navItem) =>
-//         <Link to={`/sections/${navItem.value}`} >
-//           {navItem.label}
-//         </Link>
-//       )}
-//     </Navbar>
-//   );
-// }
+export default withRouter(AppNav);
